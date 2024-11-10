94dbc9b40e70e1ebe94e3c0cbc281de0546d735e
KeitaOsaki <keita.base.05@gmail.com>
2024-11-10 21:08:14 +0900

    first commit

---
+++ app/Http/Controllers/HealthController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class HealthController extends Controller
+{
+    public function index()
+    {
+        return response()->json(['status' => 'success']);
+    }
+}
